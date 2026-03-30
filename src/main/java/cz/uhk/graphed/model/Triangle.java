121894192e96d200fee322e28a0640c580254fa0
tsukisamiyo <moondeveu@proton.me>
2026-03-30 11:57:44 +0200

    Fix triangle movement

--- src/main/java/cz/uhk/graphed/model/Triangle.java
+++ src/main/java/cz/uhk/graphed/model/Triangle.java
@@ -22,17 +22,18 @@ public class Triangle extends AbstractGraphicObject {
 
     @Override
     public void draw(Graphics g) {
+        computeC();
         Graphics2D g2d = (Graphics2D) g;
         g2d.setPaint(Color.black);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.drawLine(position.x, position.y, position.x + a, position.y);
         g2d.drawLine(position.x, position.y, cx, cy);
         g2d.drawLine(position.x + a, position.y, cx, cy);
-
     }
 
     @Override
     public boolean contains(Point p) {
-        return false;
+        int dx = (int) Math.round((position.y - p.y) / Math.tan(Math.PI / 3));
+        return p.x >= position.x + dx && p.x <= position.x + a - dx && p.y <= position.y && p.y >= cy;
     }
 }
